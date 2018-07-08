a480d2090ea62df4437f1962f3ff6c7ffc5f0c2d
Aleksandr Zotov <croessmah@ya.ru>
2018-07-08 23:13:36 +0400

    Добавлен класс ControllerAcceptor

---
+++ include/STC/ControllerAcceptor.hpp
@@ -0,0 +1,26 @@
+#ifndef INCLUDE_STC_CONTROLLERACCEPTOR_HPP
+#define INCLUDE_STC_CONTROLLERACCEPTOR_HPP
+#include <STC/Acceptor.hpp>
+#include <memory>
+#include <cstddef>
+#include <cstdint>
+
+namespace stc
+{
+
+
+class Controller;
+class ControllerAcceptor:
+        public ::stc::network::Acceptor
+{
+public:
+    ControllerAcceptor(std::shared_ptr<Controller> controller);
+    void accept(std::byte const * ptr, uint32_t size) noexcept override;
+};
+
+
+
+}//namespace stc
+
+
+#endif //INCLUDE_STC_CONTROLLERACCEPTOR_HPP
